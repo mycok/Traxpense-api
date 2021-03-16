e540fa812b6e863a53c96ce89ac949feb779ff9f
myko <awesomeme155@gmail.com>
2021-03-16 21:21:12 +0300

    feat: add wallet data repo
    - add wallet data repo

--- server/database/data-abstracts/wallet/WalletModel.ts
+++ server/database/data-abstracts/wallet/WalletModel.ts
@@ -3,5 +3,5 @@ import { Model, model } from 'mongoose';
 import { IWalletDocument } from './IWalletDocument';
 import { WalletSchema } from './WalletSchema';
 
-type IWalletModel = Model<IWalletDocument>;
-export const walletModel: IWalletModel = model('Wallet', WalletSchema);
+export type IWalletModel = Model<IWalletDocument>;
+export const WalletModel: IWalletModel = model('Wallet', WalletSchema);
